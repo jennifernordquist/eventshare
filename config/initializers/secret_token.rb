c090db8e9d5a972fd70dab826c7827f2597c4ae1
Jordan Goldstein <mrjordangoldstein@gmail.com>
2012-08-27 12:17:23 -0400

    Initial Rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventshare::Application.config.secret_token = '29015a6fbc9f06387ac2b57791b2e80c5b0c9db53694a99f327b4fb3f8585917a580261ccaf6fbf22b7503eaca26f1fb9f47ed9f0d9c608ce2bf8d5f24a5fde0'
